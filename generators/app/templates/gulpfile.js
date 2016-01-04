6171e90f162c1db8751d4a5e29c4cc13a781bb92
David Truong <david.truong@liferay.com>
2016-01-04 13:58:35 -0600

    read args pathDir and pathBuild and sets it in options

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -2,7 +2,23 @@
 
 var gulp = require('gulp');
 var liferayThemeTasks = require('liferay-theme-tasks');
+var minimist = require('minimist');
 
-liferayThemeTasks.registerTasks({
+var options = {
 	gulp: gulp
-});
+};
+
+var argv = minimist(process.argv.slice(2)); 
+
+var pathBuild = argv.pathBuild;
+var pathDist = argv.pathDist;
+
+if (pathBuild) {
+	options.pathBuild = pathBuild;
+}
+
+if (pathDist) {
+	options.pathDist = pathDist;
+}
+
+liferayThemeTasks.registerTasks(options);
